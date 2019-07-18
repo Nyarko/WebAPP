1cd16e937f01359dd7f090cb818f729eb4846a9d
Nyarko <nyarko.agyekum@yahoo.com>
2019-07-18 12:04:21 +0000

    New

--- myapp.py
+++ myapp.py
@@ -48,4 +48,4 @@ def whereami():
 #handles app properties (something like that)
 if __name__ == '__main__':
 	port = os.environ.get('PORT', 5000)
-    app.run(debug=True, host='0.0.0.0', port=port)
+app.run(debug=True, host='0.0.0.0', port=port)
